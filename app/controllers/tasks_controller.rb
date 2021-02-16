a3f00c30f48682286658db91a1c36b649a8fd3a1
wkilianh <kilian.hirt@gmail.com>
2021-02-16 16:03:57 +0100

    forgot to commit :)

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,39 @@
+class TasksController < ApplicationController
+  def index # the filename of the view has to be this, no matter what the URL is (here: /tasks)
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create # no view needed
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task[:id]) # argument replaces variable in the router (here: :id)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task) # short form of @task[:id]
+
+  end
+
+
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Task.create(title: Faker::Marketing.buzzwords, details: Faker::ChuckNorris.fact)
+end
